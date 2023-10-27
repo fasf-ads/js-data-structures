e97b6af7f4b893874284e59b0dd014eac260558e
Mateus Freira <mateus.frira@gmail.com>
2023-10-26 21:41:58 -0300

    DOJO

--- 11th-class-dojo/main.js
+++ 11th-class-dojo/main.js
@@ -1,6 +1,45 @@
 exports.Jogo = class Jogo {
-	constructor() {
-		this.jogador1 = null;
-		this.jogador2 = null;
-	}
+  constructor(){
+    this.jogador1 = {
+      pontuacao: "amor"
+     };
+
+     this.jogador2 = {
+      pontuacao: "amor"
+     };
+
+     this.pontuacao = [ "amor", 15, 30, 40];
+     
+  }
+  pontuar(jogador){
+
+     if (this.jogador1.vantagem == 0 && this.jogador2.vantagem == 0){
+      this.jogador1.vantagem = undefined; 
+      this.jogador2.vantagem = undefined;
+  
+      return
+   }
+    if (this.jogador1.pontuacao == 40 
+      && this.jogador2.pontuacao == 40 ){
+      jogador.vantagem = 0;
+    }
+    
+   
+
+    if(jogador.pontuacao == this.pontuacao[2]) {
+      jogador.pontuacao = this.pontuacao[3];
+    }
+    if(jogador.pontuacao == this.pontuacao[1]) {
+      jogador.pontuacao = this.pontuacao[2];
+    }
+    if(jogador.pontuacao == this.pontuacao[0]) {
+      jogador.pontuacao = this.pontuacao[1];
+    }
+
+    if (jogador.vantagem == 0){
+      this.vencedor = jogador;
+    }
+    
+  }
 }
+
--- 11th-class-dojo/specs.spec.js
+++ 11th-class-dojo/specs.spec.js
@@ -1,8 +1,147 @@
 const { Jogo } = require('./main');
-describe('Jogo de Tenis', function() {
-    it('deve começar sem jogadores', function() {
-        var jogo = new Jogo();
-        expect(jogo.jogador1).toBe(null);
-        expect(jogo.jogador2).toBe(null);
+describe("Jogo de tenis", function() {
+
+    it("eu deno conseguir initir o jogo", function(){
+        let meuJogo = new Jogo();
     });
+
+    it("O jogo deve ter 2 jogadores", function(){
+        let meuJogo = new Jogo();
+        expect(meuJogo.jogador1).not.toBe(null);
+        expect(meuJogo.jogador1).not.toBe(undefined);
+    });
+
+    it("se ambos os jogadores esto no love", function(){
+        let meuJogo = new Jogo();
+        expect(meuJogo.jogador1.pontuacao).toBe("amor");
+        expect(meuJogo.jogador2.pontuacao).toBe("amor");
+    });
+
+    it("se a vetor pontuacao existe", function(){
+        let meuJogo = new Jogo();
+        let arr = ["amor", 15, 30, 40];
+        expect(meuJogo.pontuacao).not.toBe(null);
+        expect(meuJogo.pontuacao).not.toBe(undefined);
+        expect(meuJogo.pontuacao.toString()).toBe(arr.toString());
+    });
+
+    it("quando o jogador1 fizer ponto ele passa para 15", function(){
+        
+        let meuJogo = new Jogo();
+        let jogador1 = meuJogo.jogador1;
+        expect(meuJogo.jogador1.pontuacao).toBe("amor");
+        meuJogo.pontuar(jogador1)
+        expect(meuJogo.jogador1.pontuacao).toBe(15);
+    });
+    it("quando o jogador1 fizer ponto ele passa para 30", function(){
+        
+        let meuJogo = new Jogo();
+        let jogador1 = meuJogo.jogador1;
+        meuJogo.pontuar(jogador1)
+        meuJogo.pontuar(jogador1)
+        expect(meuJogo.jogador1.pontuacao).toBe(30);
+    });
+    it("quando o jogador1 fizer ponto ele passa para 40", function(){
+        let meuJogo = new Jogo();
+        let jogador1 = meuJogo.jogador1;
+        meuJogo.pontuar(jogador1);
+        meuJogo.pontuar(jogador1);
+        meuJogo.pontuar(jogador1);
+        expect(meuJogo.jogador1.pontuacao).toBe(40);
+    });
+
+    it("quando o jogador1 fizer ponto ele passa para a ter uma vantagem", function(){
+        let meuJogo = new Jogo();
+        let jogador1 = meuJogo.jogador1;
+        let jogador2 = meuJogo.jogador2;
+        meuJogo.pontuar(jogador1);
+        meuJogo.pontuar(jogador1);
+        meuJogo.pontuar(jogador1);
+        meuJogo.pontuar(jogador1);
+        
+        meuJogo.pontuar(jogador2);
+        meuJogo.pontuar(jogador2);
+        meuJogo.pontuar(jogador2);
+        meuJogo.pontuar(jogador2);
+
+        expect(jogador1.pontuacao).toBe(40);
+        expect(jogador2.pontuacao).toBe(40);
+
+        meuJogo.pontuar(jogador1)
+        expect(meuJogo.jogador1.vantagem).toBe(0);
+    });
+    it("se jogador1 tiver vantagem e pontuar ele vence", function(){
+        let meuJogo = new Jogo();
+        let jogador1 = meuJogo.jogador1;
+        let jogador2 = meuJogo.jogador2;
+        meuJogo.pontuar(jogador1);
+        meuJogo.pontuar(jogador1);
+        meuJogo.pontuar(jogador1);
+        meuJogo.pontuar(jogador1);
+        
+        meuJogo.pontuar(jogador2);
+        meuJogo.pontuar(jogador2);
+        meuJogo.pontuar(jogador2);
+        meuJogo.pontuar(jogador2);
+
+        expect(jogador1.pontuacao).toBe(40);
+        expect(jogador2.pontuacao).toBe(40);
+
+        meuJogo.pontuar(jogador1)
+        expect(meuJogo.jogador1.vantagem).toBe(0);
+        meuJogo.pontuar(jogador1)
+        expect(meuJogo.vencedor).toBe(jogador1);
+    });
+
+    it("se os jogdores empaterem em vantagem, os dois perdem a vantgem", function(){
+        let meuJogo = new Jogo();
+        let jogador1 = meuJogo.jogador1;
+        let jogador2 = meuJogo.jogador2;
+        meuJogo.pontuar(jogador1);
+        meuJogo.pontuar(jogador1);
+        meuJogo.pontuar(jogador1);
+        meuJogo.pontuar(jogador1);
+        
+        meuJogo.pontuar(jogador2);
+        meuJogo.pontuar(jogador2);
+        meuJogo.pontuar(jogador2);
+        meuJogo.pontuar(jogador2);
+
+        meuJogo.pontuar(jogador1);
+        expect(meuJogo.jogador1.vantagem).toBe(0)
+        meuJogo.pontuar(jogador2);    
+
+        expect(jogador1.pontuacao).toBe(40);
+        expect(jogador2.pontuacao).toBe(40);
+        expect(jogador1.vantagem).toBe(undefined);
+        expect(jogador2.vantagem).toBe(undefined);
+    });
+
+    it("se o jogador2 pontuar depois de ter vantagem ele ganha o jogo", function(){
+        let meuJogo = new Jogo();
+        let jogador1 = meuJogo.jogador1;
+        let jogador2 = meuJogo.jogador2;
+        meuJogo.pontuar(jogador1);
+        meuJogo.pontuar(jogador1);
+        meuJogo.pontuar(jogador1);
+        meuJogo.pontuar(jogador1);
+        
+        meuJogo.pontuar(jogador2);
+        meuJogo.pontuar(jogador2);
+        meuJogo.pontuar(jogador2);
+        meuJogo.pontuar(jogador2);
+
+        meuJogo.pontuar(jogador1);
+        expect(meuJogo.jogador1.vantagem).toBe(0)
+        meuJogo.pontuar(jogador2);    
+
+        expect(jogador1.pontuacao).toBe(40);
+        expect(jogador2.pontuacao).toBe(40);
+        expect(jogador1.vantagem).toBe(undefined);
+        expect(jogador2.vantagem).toBe(undefined);
+
+        meuJogo.pontuar(jogador2);
+        expect(meuJogo.vencedor).toBe(jogador2);
+    });
+
 });
